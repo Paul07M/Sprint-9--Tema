c7c39d54defe69ba26c1591ffda1a6de8cc7fe02
Paul <lapaul86@yahoo.com>
2024-05-25 17:59:45 +0200

    exercitiu 2 terminat

---
+++ ex2.js
@@ -0,0 +1,17 @@
+//Scrieți o funcție pentru a genera un număr aleatoriu mai mare sau egal cu o variabilă start și strict mai mic decât o variabilă end . Aceste două variabile sunt capetele intervalului pentru numărul general: [start, end)
+
+//   Funcția getRandom(start, end) returnează un număr întreg aleator generat în intervalul definit de variabilele de început și sfârșit furnizate.
+
+//    Exemplu 1:
+
+//    Input: start = 3, end = 5
+
+//    Output: < un număr mai ≥ 3 și < 5>
+
+function getRandom(start, end) {
+
+    return Math.floor(Math.random() * (end - start)) + start;
+}
+
+console.log(getRandom(8, 20));
+
